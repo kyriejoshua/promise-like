02774d52cd28bb44c461d9bbf0272bb8e3939b85
kyriejoshua <zhengyuan.zhu@hipac.cn>
2019-03-27 19:07:43 +0800

    :sparkles: basic

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 * A promise library like promise.
 * node v11.2.0
+
+### Usage
+
+* A simple example.
+
+```javascript
+const promiseA = new PromiseLike((resolve, reject) => {
+  let num = 0;
+  setTimeout(() => {
+    resolve(num);
+  }, 3000);
+});
+
+promiseA.then((data) => {
+  console.info(data++)
+  return data
+})
+.then((data) => {
+  console.info(data++)
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// 既定状态
 const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
+/**
+ * 校验是否是函数
+ * @param {*} fn
+ */
 const isFunction = fn => typeof fn === 'function';
 
 export default class PromiseLike {
@@ -10,25 +15,93 @@ export default class PromiseLike {
       throw new Error('PromiseLike must accept a function as parameter!');
     }
 
+    // 默认状态和默认值
     this.status = PENDING;
     this.value = undefined;
 
+    // 在既定状态下的回调序列，当状态符合时，内部的所有函数触发
+    this.resolveCallbackQueues = [];
+    this.rejectCallbackQueues = [];
+
     try {
-      executor(this.resolve.bind(this), this.reject.bind(this));
+      executor(this.resolve, this.reject);
     } catch (error) {
       this.reject(error);
     }
   }
 
-  resolve(value) {
+  resolve = (value) => {
     if (this.status !== PENDING) { return; }
     this.status = FULFILLED;
     this.value = value;
+
+    // 符合状态时调用队列中的方法
+    let fn;
+    while (this.resolveCallbackQueues.length) {
+      fn = this.resolveCallbackQueues.shift();
+      fn(value);
+    }
   }
 
-  reject(value) {
+  reject = (value) => {
     if (this.status !== PENDING) { return; }
     this.status = REJECTED;
     this.value = value;
+
+    let fn;
+    while (this.rejectCallbackQueues.length) {
+      fn = this.rejectCallbackQueues.shift();
+      fn(value);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    const { value, status } = this;
+
+    return new PromiseLike((onFulfilledNext, onRejectNext) => {
+      const fulfilled = (data) => {
+        try {
+          if (isFunction(onFulfilled)) {
+            // 顺利流程，从上一个 promise 状态里获取结果。再传给下一个调用
+            const res = onFulfilled(data);
+            onFulfilledNext(res);
+          } else {
+            // 当 onFulfilled 不是函数的时候，但它已经是 fulfilled 状态时，直接跳过，执行当前的 resolve 回调
+            onFulfilledNext(data);
+          }
+        } catch (error) {
+          onRejectNext(error);
+        }
+      };
+
+      const rejected = (data) => {
+        try {
+          if (isFunction(onRejected)) {
+            const res = onRejected(data);
+            onRejectNext(res);
+          } else {
+            // 类同上面 resolve 里不是函数的情况，但此时将这次的值传给下个 promise.then 作为参数
+            onFulfilledNext(data);
+          }
+        } catch (error) {
+          onRejectNext(data);
+        }
+      };
+
+      switch (status) {
+        case PENDING:
+          this.resolveCallbackQueues.push(fulfilled);
+          this.rejectCallbackQueues.push(rejected);
+          break;
+        case FULFILLED:
+          fulfilled(value);
+          break;
+        case REJECTED:
+          rejected(value);
+          break;
+        default:
+          break;
+      }
+    });
   }
 }
